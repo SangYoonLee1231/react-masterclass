ace74e62048859ed0e2f9cfe1a0f1310e0fa3177
SangYoonLee <prestro1231@hufs.ac.kr>
2023-01-02 17:10:42 +0900

    ADD: ~#3.2

---
+++ react-masterclass-typescript/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div className="App">
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
+  );
+}
+
+export default App;
