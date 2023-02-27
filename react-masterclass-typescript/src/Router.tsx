80d57618b3cd002c66156de1195f1ee7357280c3
SangYoonLee <prestro1231@hufs.ac.kr>
2023-02-27 16:54:17 +0900

    Modify

--- react-masterclass-typescript/src/Router.tsx
+++ react-masterclass-typescript/src/Router.tsx
@@ -5,6 +5,7 @@ import User from "./screens/users/User";
 import Root from "./Root";
 import NotFound from "./components/NotFound";
 import ErrorComponent from "./components/ErrorComponent";
+import Followers from "./screens/users/Followers";
 
 const router = createBrowserRouter([
   {
@@ -22,13 +23,13 @@ const router = createBrowserRouter([
         errorElement: <ErrorComponent />,
       },
       {
-        path: "users",
+        path: "users/:userId",
         element: <User />,
         errorElement: <ErrorComponent />,
         children: [
           {
-            path: ":userId",
-            element: <User />,
+            path: "followers",
+            element: <Followers />,
           },
         ],
       },
--- react-masterclass-typescript/src/screens/users/User.tsx
+++ react-masterclass-typescript/src/screens/users/User.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Outlet, Link } from "react-router-dom";
 import { users } from "../../db";
 
 function User() {
@@ -6,9 +6,14 @@ function User() {
 
   return (
     <div>
-      <h1>User</h1>
-      <h3>ID : {userId}</h3>
-      <h3>Name : {users[Number(userId) - 1].name}</h3>
+      <div>
+        <h1>User</h1>
+        <h3>ID : {userId}</h3>
+        <h3>Name : {users[Number(userId) - 1].name}</h3>
+      </div>
+      <hr />
+      <Outlet />
+      <Link to="followers">See Followers</Link>
     </div>
   );
 }
