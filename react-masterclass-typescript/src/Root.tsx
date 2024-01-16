ac1c79a7008dfa1db2c9ca3a02921cad91cffe6e
SangYoonLee <prestro1231@hufs.ac.kr>
2024-01-17 00:49:45 +0900

    Add: '#3.5 Form'까지 실습 진행

--- react-masterclass-typescript/src/Root.tsx
+++ react-masterclass-typescript/src/Root.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import App from "./App";
 
 function Root() {
   return (
     <div className="Root">
       <div>
         <Header />
+        <App />
         <Outlet />
       </div>
     </div>
