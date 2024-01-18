67cb28385b08a1ddde8a6407403309510d6a8cb2
SangYoonLee <prestro1231@hufs.ac.kr>
2024-01-18 19:14:54 +0900

    Modify: '#8 Animation' 실습을 위한 초기 세팅

--- react-masterclass-typescript/src/styled.d.ts
+++ react-masterclass-typescript/src/styled.d.ts
@@ -1,10 +1,5 @@
-// import original module declarations
 import "styled-components";
 
-// and extend them!
 declare module "styled-components" {
-  export interface DefaultTheme {
-    textColor: string;
-    bgColor: string;
-  }
+  export interface DefaultTheme {}
 }
